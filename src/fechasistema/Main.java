0d93c65eb0e9948909ff54045f76192a46325a59
Xtek <Xtek@Pc-Nasa>
2019-10-25 19:22:17 +0200

    :tada: Commit inicial

---
+++ src/fechasistema/Main.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright 2019 Alejandro Alberca - alejandro.alberca.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fechasistema;
+import java.util.*;
+/**
+ *
+ * @author Alejandro Alberca - alejandro.alberca.alum@iescamp.es
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        int d = cal.get(Calendar.DAY_OF_MONTH);
+        int m = cal.get(Calendar.MONTH);
+        int y = cal.get(Calendar.YEAR);
+        System.out.printf("Fecha del sistema ...: %02d:%02d:%d ", d, m, y);
+    }
+}
